14fe9ce0a9bcdabe9d98d7e07a33e5f606d61a2a
Ian Littman <iansltx@gmail.com>
2021-10-16 09:11:08 -0500

    Match input file format to 2021 site

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Based off of work by Chris Cornutt aka enygma. Thanks!
 
 1. Clone
 2. `composer install`
-3. Copy the ti.to check-in list for sponsor bingo to checked-in.csv
+3. From the Longhorn PHP Laravel app, save the results of `php artisan longhornphp:bingo:completed` to eligible.tsv
 4. `php -S 0.0.0.0:9000`
 5. Access at `localhost:9000`
 
 Refresh to go through another iteration of the raffle. Winners, stored
-in longhornphp19-filter.csv, will be skipped in successive rounds.
+in raffle-filter.csv, will be skipped in successive rounds.
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'vendor/autoload.php';
 
-// expects Tito CSV output
-$file = __DIR__.'/checked-in.csv';
-$filter = __DIR__.'/longhornphp19-filter.csv';
+// expects output from php artisan longhornphp:bingo:completed
+$file = __DIR__ . '/eligible.tsv';
+$filter = __DIR__ . '/raffle-filter.csv';
 
 $app = new \Slim\App();
 
@@ -11,14 +11,15 @@ $app->get('/', function($request, $response)  {
     include_once(__DIR__.'/template/index.php');
 });
 $app->get('/rand', function($request, \Slim\Http\Response $response) use ($file, $filter){
-    $names = [];
+    $people = [];
     $row = 0;
 
     // open the file
-    if (($handle = fopen($file, "r")) !== FALSE) {
-        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            if ($row > 0) {
-                $names[] = trim($data[3]) . ' ' . trim($data[4]);
+    if (($handle = fopen($file, "r")) !== FALSE) { // email, first, last, ticket type (in-person/virtual)
+        while (($line = fgets($handle, 1000)) !== FALSE) {
+            $data = explode("\t", $line);
+            if ($row > 0 && count($data) >= 4) {
+                $people[] = ['email' => $data[0], 'name' => "$data[1] $data[2]", 'type' => $data[3]];
             }
             $row++;
         }
@@ -27,21 +28,21 @@ $app->get('/rand', function($request, \Slim\Http\Response $response) use ($file,
 
     // filter any names from the list that have won already or aren't there
     $winners = file_exists($filter) ? file($filter) : [];
-    foreach ($names as $n_index => $name) {
+    foreach ($people as $n_index => $person) {
         foreach ($winners as $winner) {
-            if (trim($winner) == trim($name)) {
-                unset($names[$n_index]);
+            if (trim($winner) == trim($person['name'])) {
+                unset($people[$n_index]);
             }
         }
     }
 
     // pick a random one
-    $name = $names[array_rand($names)];
-    return $response->withJson(['name' => $name]);
+    $currentWinner = $people[array_rand($people)];
+    return $response->withJson(['name' => $currentWinner['name']]);
 });
 $app->post('/name', function($request, $response) use ($filter){
     $name = $request->getParam('name');
-    file_put_contents($filter, trim($name)."\n", FILE_APPEND);
+    file_put_contents($filter, trim($name) . "\n", FILE_APPEND);
 });
 
 $app->run();
